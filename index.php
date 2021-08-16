5d1e600e1b4622c78e8639672428e1524a365599
ryan-the-SWE <software.engineer.ryan@gmail.com>
2021-08-16 13:02:09 -0700

    Create index.php file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="/assets/css/index.css">
+</head>
+<body>
+    <main>
+        <?php include("navigation.php") ?><br/>
+        <?php include("footer.php") ?>
+    </main>
+</body>
+</html>
